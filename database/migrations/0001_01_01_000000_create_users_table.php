4cdccb706788d50ef19b4452837aa1b0d795bd76
niicaa17 <nandachairunnis17@gmail.com>
2025-08-05 14:10:21 +0700

    fector table WaterLog

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('daily_target')->nullable();
+            $table->intger('daily_target')->default(0);
             $table->rememberToken();
             $table->timestamps();
         });
--- database/migrations/2025_08_05_070528_create_water_logs_table.php
+++ database/migrations/2025_08_05_070528_create_water_logs_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('water_logs', function (Blueprint $table) {
             $table->id();
+            $table ->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->integer('amount')-> default(0);
+            $table->date('log_date');
             $table->timestamps();
         });
     }
